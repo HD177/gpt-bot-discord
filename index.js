94a1d2ad650e460a3cc90f0f8dd1c29bd30a7dfe
Harrison Dempsey <hi@hdempsey.io>
2022-12-25 23:45:37 +1100

    More cleanup

--- index.js
+++ index.js
@@ -5,10 +5,6 @@ const dotenv = require('dotenv');
 // Load environment variables from .env file
 dotenv.config();
 
-// The fs module is Node's native file system module. fs is used to read the commands directory and identify our command files.
-// The path module is Node's native path utility module. path helps construct paths to access files and directories. One of the advantages of the path module is that it automatically detects the operating system and uses the appropriate joiners.
-// The Collection class extends JavaScript's native Map class, and includes more extensive, useful functionality. Collection is used to store and efficiently retrieve commands for execution.
-
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
