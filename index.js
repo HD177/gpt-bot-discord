12f7789f7bfd47ed0237df50a8c59e11b27ae5a6
Harrison Dempsey <hi@hdempsey.io>
2023-01-05 16:00:07 +1100

    hacky fix for channel-specific responses

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-// Require the necessary discord.js classes
+// Require the necessary discord.js, openai, and dotenv modules
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { Configuration, OpenAIApi } = require("openai");
 const dotenv = require('dotenv');
 
 // Load environment variables from .env file
@@ -8,28 +9,47 @@ dotenv.config();
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
+// When the client is ready, run this code (only once)
+// 'c' is used for the event parameter to keep it separate from the already defined 'client'
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
 // Sets up OpenAI API with credentials
-const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+    apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
 // On message event, take input as prompt and generate response
+
+
+
 client.on("messageCreate", function (message) {
-  if (message.author.bot) return;
-  let prompt = message.content;
-  (async () => {
-       const gptResponse = await openai.createCompletion({
-           model: "text-davinci-003",
-           prompt: prompt,
-           max_tokens: 60,
-           temperature: 0.3,
-          //  top_p: 0.3,
-           presence_penalty: 0,
-           frequency_penalty: 0.5,
-         });
-       message.reply(`${gptResponse.data.choices[0].text.substring(5)}`);
-       prompt += `${gptResponse.data.choices[0].text}\n`;
-   })();
+    if (message.channel.name !== 'gpt-gen') {
+    return;
+    } else {
+        if (message.author.id === client.user.id) {
+        return;
+    } else {
+        // Send request to OpenAI API
+        const prompt = message.content;
+        console.log(prompt + '-given');
+        (async () => {
+            const gptResponse = await openai.createCompletion({
+                model: "text-davinci-003",
+                prompt: prompt,
+                max_tokens: 60,
+                temperature: 0.3,
+                //  top_p: 0.3,
+                presence_penalty: 0,
+                frequency_penalty: 0.5,
+            });
+            console.log(gptResponse.data.choices[0].text);
+            message.reply(`${gptResponse.data.choices[0].text}`);
+        })();
+    }
+    }
 });
+
+client.login(process.env.token);
